16fee9847a8ef2ab86f2501b0153199a09919cc1
Leonti Bielski <leonti.bielski@myob.com>
2018-07-04 13:01:52 +1000

    Fix stack location

--- build-binary.sh
+++ build-binary.sh
@@ -12,12 +12,12 @@ if [ $# -eq 1 ] && ([ "$1" == '--help' ] || [ "$1" == '-h' ]); then
 fi
 
 echo "Building static binary"
-/root/.local/bin/stack --version
-/root/.local/bin/stack setup --allow-different-user
-/root/.local/bin/stack build --allow-different-user --ghc-options "-optl-static -threaded -fPIC -optc-Os" -- .
+stack --version
+stack setup --allow-different-user
+stack build --allow-different-user --ghc-options "-optl-static -threaded -fPIC -optc-Os" -- .
 
 # Strip all statically linked executables
-find "$(/root/.local/bin/stack path --allow-different-user --dist-dir)/build" \
+find "$(stack path --allow-different-user --dist-dir)/build" \
   -type f \
   -perm -u=x,g=x,o=x \
   -exec strip --strip-all --enable-deterministic-archives --preserve-dates {} +
